b6af0dcd0a55922bf21edebc67aa4e2a7bc107fb
hemanthsoma <39868208+hemanthsoma@users.noreply.github.com>
2020-09-24 18:52:54 +0530

    Create FourPlayersScoreCombination.py

---
+++ FourPlayersScoreCombination.py
@@ -0,0 +1,32 @@
+'''
+The program must accept the score of N players in a cricket match. The program must print the number of
+combinations with exactly 4 players and total runs scored by the 4 players is greater than or equal to 100 as the
+output.
+Boundary Condition(s):
+4 <= N <= 250
+Input Format:
+The first line contains N.
+The second line contains N integers separated by space.
+Output Format:
+The first line contains the number of combinations as mentioned above.
+Example Input/Output 1:
+Input:
+6
+20 50 20 10 30 25
+Output:
+10
+Example input/output 2:
+Input:
+8
+32 11 40 60 26 23 66 43
+Output:
+69
+'''
+
+from itertools import combinations
+n=int(input());c=0
+l=list(map(int,input().split()))
+for i in list(combinations(l,4)):
+    if sum(i)>=100:
+        c+=1
+print(c)

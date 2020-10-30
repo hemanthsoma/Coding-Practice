97c975adb2e4a28bff6b289dae1232e00487d2f0
h3m7T <39868208+hemanthsoma@users.noreply.github.com>
2020-10-30 21:36:14 +0530

    Update Xpattern.py

--- Xpattern.py
+++ Xpattern.py
@@ -15,3 +15,15 @@ for i in range(1,n+1):
     Xpattern(i)
 for i in range(n-1,0,-1):
     Xpattern(i)
+'''
+5
+1       1
+ 2     2 
+  3   3  
+   4 4   
+    5    
+   4 4   
+  3   3  
+ 2     2 
+1       1
+'''

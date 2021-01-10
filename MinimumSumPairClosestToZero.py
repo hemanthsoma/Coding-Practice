3419880d7470100ca9c7d3f9a237f8d16b68c46a
h3m7T <39868208+hemanthsoma@users.noreply.github.com>
2021-01-10 09:28:42 +0530

    Update MinimumSumPairClosestToZero.py

--- MinimumSumPairClosestToZero.py
+++ MinimumSumPairClosestToZero.py
@@ -1,3 +1,13 @@
+'''
+This problem is also called minimum absolute sum pair. You are given a number N followed by an array of N numbers, containing both +ve and -ve numbers. You need to print the two elements such that their sum is closest to zero.
+Input Size : N <= 100000
+Sample Testcase :
+INPUT
+5
+15 5 -20 30 -45
+OUTPUT
+15 -20
+'''
 from itertools import combinations
 n=int(input())
 l=list(map(int,input().split()))

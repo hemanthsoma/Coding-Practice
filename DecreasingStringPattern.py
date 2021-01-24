d71a139d8a2aa23c29b83392a0c4df857c9a6d25
h3m7T <39868208+hemanthsoma@users.noreply.github.com>
2021-01-24 13:01:10 +0530

    Update DecreasingStringPattern.py

--- DecreasingStringPattern.py
+++ DecreasingStringPattern.py
@@ -1,10 +1,8 @@
 n=input()
-print(n)
-c=1;l=len(n)//2-1;d=0;g=len(n)//2+1
-for i in range(len(n)//2-1):
+c=0;d=0;l=len(n)//2;g=l
+for i in range(len(n)//2+1):
     print('*'*c+n[d:l]+n[g:len(n)]+'*'*c)
     l-=1;c+=1;g+=1
-print('*'*len(n))
 '''
 barber
 barber

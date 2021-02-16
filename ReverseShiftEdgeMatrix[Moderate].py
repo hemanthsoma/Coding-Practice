9016645661b41a6f18f3bc4089f64ae61bef7da7
h3m7T <39868208+hemanthsoma@users.noreply.github.com>
2021-02-16 21:27:52 +0530

    Create ReverseShiftEdgeMatrix[Moderate].py

---
+++ ReverseShiftEdgeMatrix[Moderate].py
@@ -0,0 +1,37 @@
+a,b=list(map(int,input().split()))
+l=[]
+for i in range(a):
+    l.append(list(map(str,input().strip().split())))
+for i in range(a):
+    for j in range(b):
+        if i==0 and j==0:
+            print(int(l[1][0][::-1]),end=' ')
+        elif i==0 and j!=0:
+            print(int(l[i][j-1][::-1]),end=' ')
+        elif i==a-1 and j==b-1:
+            print(int(l[a-2][b-1][::-1]),end=' ')
+        elif i==a-1:
+            print(int(l[i][j+1][::-1]),end=' ')
+        elif j==0:
+            if i!=0 and i!=a-1:
+                print(int(l[i+1][j][::-1]),end=' ')
+        elif j==b-1:
+            if i!=0 and i!=a-1:
+                print(int(l[i-1][j][::-1]),end=' ')
+        else:
+            print(l[i][j],end=' ')
+    print()
+    
+'''
+Input:
+4 4
+85 84 12 26
+33 43 91 95
+98 17 45 66
+57 17 73 10
+Output:
+33 58 48 21 
+89 43 91 62 
+75 17 45 59 
+71 37 1 66 
+'''
